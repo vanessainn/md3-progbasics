21c0b121fce19267f050140acf8eb3808088c0ce
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2023-11-15 13:59:35 +0100

    update homework.

--- 231108_aufgabe/js/script.js
+++ 231108_aufgabe/js/script.js
@@ -1,56 +1,62 @@
 'use strict';
 
 let text = '';
+let img = '';
 
 switch (new Date().getMonth()) {
     case 0:
-        document.getElementById('output_image').src = 'images/januar.jpeg';
+        img = 'images/januar.jpeg';
         text = 'Januar';
         break;
     case 1:
-        document.getElementById('output_image').src = 'images/februar.jpeg';
+        img = 'images/februar.jpeg';
         text = 'Februar';
         break;
     case 2:
-        document.getElementById('output_image').src = 'images/märz.jpeg';
+        img = 'images/märz.jpeg';
         text = 'März';
         break;
     case 3:
-        document.getElementById('output_image').src = 'images/april.jpeg';
+        img = 'images/april.jpeg';
         text = 'April';
         break;
     case 4:
-        document.getElementById('output_image').src = 'images/mai.jpeg';
+       img = 'images/mai.jpeg';
         text = 'Mai';
         break;
     case 5:
-        document.getElementById('output_image').src = 'images/juni.jpeg';
+        img = 'images/juni.jpeg';
         text = 'Juni';
         break;
     case 6:
-        document.getElementById('output_image').src = 'images/juli.jpeg';
+        img = 'images/juli.jpeg';
         text = 'Juli';
         break;
     case 7:
-        document.getElementById('output_image').src = 'images/august.jpeg';
+        img = 'images/august.jpeg';
         text = 'August';
         break;
     case 8:
-        document.getElementById('output_image').src = 'images/september.jpeg';
+        img = 'images/september.jpeg';
         text = 'September';
         break;
     case 9:
-        document.getElementById('output_image').src = 'images/oktober.jpeg';
+        img = 'images/oktober.jpeg';
         text = 'Oktober';
         break;
     case 10:
-        document.getElementById('output_image').src = 'images/november.jpeg';
+        img = 'images/november.jpeg';
         text = 'November';
         break;
     case 11:
-        document.getElementById('output_image').src = 'images/dezember.jpeg';
+        img = 'images/dezember.jpeg';
         text = 'Dezember';
+        break;
+    default:
+        img = 'images/nomonth.jpeg';
+        text = 'It is not a month.';
         break;
 }
 
+document.getElementById('output_image').src = img;
 document.getElementById('output_title').innerHTML = text;
